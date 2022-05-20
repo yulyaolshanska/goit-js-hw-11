a29e03bdd5af0e93d2a957544291753d217fd379
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-05-20 21:19:55 +0300

    Порядок в коде
    1. Чистка кода.
    2. Refresh для SimpleLightbox.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import ImagesApiService from "./js/api-service"
 
-const imagesApiService = new ImagesApiService()
+
 const refs = {
     searchForm: document.querySelector("#search-form"),
     searchBtn: document.querySelector("button"),
@@ -15,9 +15,9 @@ const refs = {
     galleryBox: document.querySelector(".gallery"),
 }
 
-
-
+const imagesApiService = new ImagesApiService()
 let imgsArray = [];
+let gallery = null;
 
 refs.searchForm.addEventListener("submit", onFormSubmit);
 refs.loadMoreBtn.addEventListener("click", onLoadMore);
@@ -25,55 +25,52 @@ refs.galleryBox.addEventListener("click", onGalleryItemClick)
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  refs.galleryBox.innerHTML = "";
-  refs.loadMoreBtn.classList.add("-is-hidden");
-  
+  clearImgContainer()
   imagesApiService.query = evt.currentTarget.elements.query.value;
-  if (ImagesApiService.query === "") {
+  if (imagesApiService.query === "") {
+     Notiflix.Notify.warning(`Please, enter your request in the input field`);
     return
   } else {
     imagesApiService.resetPage()
     imagesApiService.fetchImages().then(images => {
     imgsArray = images.hits;
-    
-      if (imgsArray.length === 0) {
-      showMessage()
+    if (imgsArray.length === 0) {
+        showMessage()
     } else {
-      appendImagesMarkup();
+        appendImagesMarkup(imgsArray);
+        newSimpleLightbox()
     }
-    })
+    }).catch(error => console.log(error));
   }
   
 }
 
-function appendImagesMarkup() {
-   refs.galleryBox.insertAdjacentHTML("beforeend", renderGalleryMarkup(imgsArray));
-      refs.loadMoreBtn.classList.remove("-is-hidden");
-      let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: "alt",
-    captionDelay: 250,
-});
-}
-
-function showMessage() {
-
-       refs.loadMoreBtn.classList.add("-is-hidden");
-      Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
-}
-
 function onLoadMore() {
   imagesApiService.incrementPage();
-
   imagesApiService.fetchImages().then(images => {
-   
-    imgsArray = images.hits;
-    
-    refs.galleryBox.insertAdjacentHTML("beforeend", renderGalleryMarkup(imgsArray));
-     
-    // gallery.refresh();
+   imgsArray = images.hits;
+   refs.galleryBox.insertAdjacentHTML("beforeend", renderGalleryMarkup(imgsArray));
+   refreshSimpleLightbox();
   })
  
 }
+
+function clearImgContainer() {
+ refs.galleryBox.innerHTML = "";
+  refs.loadMoreBtn.classList.add("-is-hidden");
+}
+
+function appendImagesMarkup(array) {
+   refs.galleryBox.insertAdjacentHTML("beforeend", renderGalleryMarkup(array));
+   refs.loadMoreBtn.classList.remove("-is-hidden");
+    
+}
+
+function showMessage() {
+    Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+}
+
+
 
 function renderGalleryMarkup(images) {
   return images.map(image => 
@@ -93,15 +90,25 @@ function renderGalleryMarkup(images) {
       
     </li>
   </ul></div>
-`
-
-    ).join("")
+`).join("")
 }
+
 function onGalleryItemClick(event) {
    event.preventDefault();
   const isGalleryImage = event.target.classList.contains("gallery-img");
   if (!isGalleryImage) {
     return;
   }
+}
+
+function newSimpleLightbox() {
+  gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+}
+
+function refreshSimpleLightbox() {
+  gallery.refresh()
 }
 
